7fde95c409a42032799025af46c5c857109167b5
哈斯普拉提 <1498893392@qq.com>
2019-05-25 00:23:48 +0800

    Create the CenterInformation layout.Ref #6

--- src/views/center/CenterInformation.vue
+++ src/views/center/CenterInformation.vue
@@ -1,94 +1,69 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <a-tabs defaultActiveKey="1" @change="change_course">
+      <a-tab-pane v-for="course in courses" :tab="course.name" :key="course.index">
+        <p>编译错误: {{ course.compiler_err }}</p>
+        <p>答案错误: {{ course.answer_err }}</p>
+        <p>运行错误: {{ course.runtime_err }}</p>
+        <p>时间超线: {{ course.timeout }}</p>
+        <p>其他错误: {{ course.other_err }}</p>
+        <p>待添加饼图表格类插件</p>
+      </a-tab-pane>
+    </a-tabs>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'HelloWorld',
+  name: 'Infomation',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      courses: [{
+        name: '数据结构',
+        index: '1',
+        compiler_err: '123',
+        answer_err: '123',
+        runtime_err: '123',
+        timeout: '123',
+        other_err: '123'
+      },
+      {
+        name: '算法导论',
+        index: '2',
+        compiler_err: '123',
+        answer_err: '123',
+        runtime_err: '123',
+        timeout: '123',
+        other_err: '123'
+      },
+      {
+        name: 'JAVA应用',
+        index: '3',
+        compiler_err: '123',
+        answer_err: '123',
+        runtime_err: '123',
+        timeout: '123',
+        other_err: '123'
+      }]
+    }
+  },
+  method: {
+    change_course (key) {
+      console.log(key)
+    },
+    function () {
+      axios.get('/api/student/course')
+      .then(function(responce) {
+        this.courses = responce
+        for(let i = 0; i < responce.len(); i++){
+          this.courses[i].index = i + 1
+        }
+      })
+      .catch(function(err) {
+        console.log(err)
+      })
     }
   }
 }
