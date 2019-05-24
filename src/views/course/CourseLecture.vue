44c964127d9dd21b0fa7d881fd22a49d46d8e7ca
mobaixin <13687162414@163.com>
2019-05-25 00:16:18 +0800

    course_sourse

--- src/views/course/CourseLecture.vue
+++ src/views/course/CourseLecture.vue
@@ -1,101 +1,90 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
+  <div>
+    <a-form 
+      layout="inline" 
+      :form="form" 
+      @submit="handleSubmit"
+    >
+      <a-form-item 
+        :validate-status="sectionNameError() ? 'error' : ''"
+        :help="sectionNameError() || ''"
+      >
+        <a-input 
+          v-decorator="[ 'sectionName',
+            {rules: [{ required: true, message: '请输入章节名称!' }]}
+          ]"
+          placeholder="章节名称"
         >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <!-- <a-button 
+          type="primary" 
+          html-type="submit"
+          :disabled="hasErrors(form.getFieldsError())
         >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+          搜索
+        </a-button> -->
+      </a-form-item>
+    </a-form>                                           
+
+    <a-table :columns="columns" :dataSource="data">
+      <a slot="action" href="javascript:;">Delete</a>
+      <p slot="expandedRowRender" slot-scope="record" style="margin: 0">{{record.description}}</p>
+    </a-table>
   </div>
 </template>
 
 <script>
+const columns = [
+  { title: 'Name', dataIndex: 'name', key: 'name' },
+  { title: 'Age', dataIndex: 'age', key: 'age' },
+  { title: 'Address', dataIndex: 'address', key: 'address' },
+  { title: 'Action', dataIndex: '', key: 'x', scopedSlots: { customRender: 'action' } },
+]
+
+const data = [
+  { key: 1, name: 'John Brown', age: 32, address: 'New York No. 1 Lake Park', description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.' },
+  { key: 2, name: 'Jim Green', age: 42, address: 'London No. 1 Lake Park', description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.' },
+  { key: 3, name: 'Joe Black', age: 32, address: 'Sidney No. 1 Lake Park', description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.' },
+]
+function hasErrors (fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field])
+}
 export default {
-  name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      hasErrors,
+      form: this.$form.createForm(this),
+      data,
+      columns,
     }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      // To disabled submit button at the beginning.
+      this.form.validateFields()
+    })
+  },
+  methods: {
+    sectionNameError () {
+      const { getFieldError, isFieldTouched } = this.form
+      return isFieldTouched('userName') && getFieldError('userName')
+    },
+    handleSubmit  (e) {
+      e.preventDefault()
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values)
+        }
+      })
+    },
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<!-- <style scoped>
 h1, h2 {
   font-weight: normal;
 }
@@ -110,4 +99,18 @@ li {
 a {
   color: #42b983;
 }
-</style>
+.text_10247X6 {
+	width: 320px;
+	height: 38px;
+	left: 321px;
+	top: 241px;
+	text-indent: 5px;
+	color: rgba(51, 51, 51, 1);
+	background-color: rgba(240, 242, 245, 1);
+	border-radius: 2px;
+	font-size: 18px;
+	line-height: 150%;
+	border: rgba(153, 153, 153, 1) solid 1px;
+	text-align: left;
+}
+</style> -->
