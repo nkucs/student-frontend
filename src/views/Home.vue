134015d40495ecd9414a812a6b0639589168a9e2
[zcyyc] <[741339859@qq.com]>
2019-05-27 19:12:03 +0800

    首页添加Echarts图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,7 +52,19 @@
                     </div>
                 </a-card>
                 <a-card>
-                    <p>题目统计</p>
+                    <a-divider> 提交记录 </a-divider>
+                    <template>
+                    <div style="margin-left:-150px;">
+                    <ve-pie :data="chartData"></ve-pie>
+                    </div>
+                    </template>
+                    <a-divider> 每日提交情况 </a-divider>
+                    <template>
+                    <div style="margin-left:350px; width:600px;">
+                    <ve-line :data="chartData2" :settings="chartSettings"></ve-line>
+                    </div>
+                    </template>
+
                 </a-card>
             </a-layout-content>
             <a-layout-sider>个人信息的侧边栏</a-layout-sider>
@@ -60,6 +72,7 @@
         </a-layout>
     </div>
 </template>
+
 <style scoped>
 .title {
     font-size: 20px;
@@ -96,6 +109,32 @@
 </style>
 <script>
 export default {
+    data() {
+        return {
+        chartData: {
+            columns: ['日期', '访问用户'],
+            rows: [
+                { '日期': 'Accept', '访问用户': 1393 },
+                { '日期': '其他', '访问用户': 3530 },
+                { '日期': '答案错误', '访问用户': 2923 },
+                { '日期': '编译错误', '访问用户': 1723 },
+                { '日期': '时间或空间超限', '访问用户': 4593 }
+            ]
+        },
+        chartData2: {
+            columns: ['日期', '提交次数'],
+            rows: [
+                { '日期': '1', '提交次数': 1393},
+                { '日期': '2', '提交次数': 3530},
+                { '日期': '3', '提交次数': 2923},
+                { '日期': '4', '提交次数': 1723},
+                { '日期': '5', '提交次数': 3792},
+                { '日期': '6', '提交次数': 4593},
+                { '日期': '7', '提交次数': 4593}
+            ]
+        }
+    }
+  }
     
 }
 </script>
