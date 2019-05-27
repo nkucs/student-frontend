db70b028c4a4f73b559501578b059b396c486825
[zcyyc] <[741339859@qq.com]>
2019-05-27 09:21:31 +0800

    更新提交记录页与提交结果页

--- src/views/center/CenterSubmission.vue
+++ src/views/center/CenterSubmission.vue
@@ -1,4 +1,5 @@
-<template>
+
+<!--<template>
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
@@ -92,10 +93,10 @@ export default {
     }
   }
 }
-</script>
+</script>-->
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<!--<style scoped>
 h1, h2 {
   font-weight: normal;
 }
@@ -110,4 +111,99 @@ li {
 a {
   color: #42b983;
 }
+</style>-->
+<template>
+  <a-table :columns="columns" :dataSource="data" @change="onChange" />
+</template>
+
+<script>
+import classnames from 'classnames';
+import { ListSize } from 'ant-design-vue/es/list';
+const columns = [{
+  title: '题目编号',
+  dataIndex: 'id', 
+  align:'center',
+  sorter: (a, b) => a.id - b.id,
+}, {
+  title: '提交时间',
+  dataIndex: 'time',
+  align:'center',
+  sorter: (a, b) => a.time - b.time,
+}, {
+  title: '通过情况',
+  dataIndex: 'accept',
+  align:'center',
+  sorter: (a, b) => a.accept - b.accept,
+}, {
+  title: '分数',
+  dataIndex: 'score',
+  align:'center',
+  sorter: (a, b) => a.score - b.score,
+}, {
+  title: '代码长度',
+  dataIndex: 'length',
+  align:'center',
+  sorter: (a, b) => a.length - b.length,
+}, {
+  title: '语言',
+  dataIndex: 'language',
+  align:'center',
+  
+}, {
+  title: '运行时间',
+  dataIndex: 'run',
+  align:'center',
+  sorter: (a, b) => a.run - b.run,
+},{
+  title: '使用内存',
+  dataIndex: 'store',
+  align:'center',
+  sorter: (a, b) => a.store - b.store,
+},{
+  title: '状态',
+  dataIndex: 'state',
+  align:'center',
+},
+
+];
+
+const data = [{
+  key: '1',
+  id: 1001,
+  time: "2019-01-01 23:59:59",
+  accept: 0.2,
+  score:20,
+  length:936,
+  language:'C++',
+  run:123,
+  store:2.5,
+  state:'内存超限',
+  
+}];
+
+function onChange(pagination, sorter) {
+  console.log('params', pagination, sorter);
+}
+
+
+export default {
+  data() {
+    return {
+      data,
+      columns,
+    }
+  },
+  methods: {
+    onChange,
+  }
+}
+</script>
+
+<style>
+.ant-table{
+  font-size: 14px;
+}
 </style>
+
+
+
